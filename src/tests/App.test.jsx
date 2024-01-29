6b8fde23ab4d035647c09543df9597e7d25ccdc1
Grant Collins <grantcollins03@gmail.com>
2024-01-29 11:36:41 -0500

    installed and setup react router

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { render, screen } from "@testing-library/react";
-import App from "../App";
+import App from "../components/App";
 
 describe("App component", () => {
     it("renders correct heading", () => {
