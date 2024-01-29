a607a63fd69904ca8d1d61105c57c8b0aae0decd
Grant Collins <grantcollins03@gmail.com>
2024-01-29 15:40:46 -0500

    setup Product Page route

---
+++ src/components/ProductsPage.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+const ProductsPage = () => {
+    return (
+        <>
+            <h1>Products Page</h1>
+            <Link to='/'>Go home?</Link>
+        </>
+    )
+}
+
+export default ProductsPage
