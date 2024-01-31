deb9aec81c2ab184204b5522ac4829b6a80f539e
Grant Collins <grantcollins03@gmail.com>
2024-01-31 12:24:06 -0500

    restyled logo

--- src/components/reusables/Logo.jsx
+++ src/components/reusables/Logo.jsx
@@ -3,23 +3,21 @@ import { Link } from 'react-router-dom'
 import cartIcon from '../../assets/icons/shopping-cart.svg'
 
 const StyledLogoLink = styled(Link)`
-    text-decoration: none;
-    color: black;
     display: flex;
     align-items: center;
     gap: 4px;
-    font-size: 2rem;
+`
 
-    &:hover {
-        opacity: 0.6;
-    }
+const LogoText = styled.p`
+    font-size: 1.6rem;
+    font-weight: 700;
 `
 
 const Logo = () => {
     return (
         <StyledLogoLink to='/'>
             <img src={cartIcon} alt='cart icon' />
-            <p>CartMart</p>
+            <LogoText>CartMart</LogoText>
         </StyledLogoLink>
     )
 }
