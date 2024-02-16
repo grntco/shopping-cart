b0870347a5b1b58c83d52e0deb9993d8d785fc55
Grant Collins <grantcollins03@gmail.com>
2024-02-16 13:52:03 -0500

    fixed App.test.jsx to include BrowserRouter

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,27 +2,6 @@ import { Link } from 'react-router-dom'
 import Logo from './reusables/Logo'
 import styled from 'styled-components'
 
-const StyledFooter = styled.footer`
-    display: flex;
-    justify-content: space-between;
-    padding: 48px 96px;
-    background-color: black;
-`
-
-const FooterLinks = styled.ul`
-    display: flex;
-    flex-direction: column;
-    gap: 4px;
-`
-
-const FooterLinkItem = styled.li`
-    list-style: none;
-`
-
-const FooterLink = styled(Link)`
-    color: white;
-`
-
 const Footer = () => {
     return (
         <StyledFooter data-testid='footer'>
@@ -47,5 +26,26 @@ const Footer = () => {
         </StyledFooter>
     )
 }
+
+const StyledFooter = styled.footer`
+    display: flex;
+    justify-content: space-between;
+    padding: 48px 96px;
+    background-color: black;
+`
+
+const FooterLinks = styled.ul`
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+`
+
+const FooterLinkItem = styled.li`
+    list-style: none;
+`
+
+const FooterLink = styled(Link)`
+    color: white;
+`
 
 export default Footer
--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,14 +1,13 @@
 import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import App from '../components/App'
-import Router from '../components/Router'
+import { BrowserRouter } from 'react-router-dom'
 
 describe('App component', () => {
     it('renders header, main, and footer', () => {
-        render(<Router><App /></Router>)
-        // expect(true).toBeTruthy()
-        // expect(screen.getByRole('main')).toBeInTheDocument()
-        // expect(screen.getByRole('main')).toBeInTheDocument()
+        render(<App />, { wrapper: BrowserRouter })
+        expect(screen.getByTestId('header')).toBeInTheDocument()
+        expect(screen.getByRole('main')).toBeInTheDocument()
         expect(screen.getByTestId('footer')).toBeInTheDocument()
     })
 })
