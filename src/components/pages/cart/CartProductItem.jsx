588c185b9562e24e20eeba91117cc0bf7d1d4919
Grant Collins <grantcollins03@gmail.com>
2024-02-23 08:26:02 -0500

    pass test: trash btn calls handleDeleteFromCart

--- src/components/pages/cart/CartProductItem.jsx
+++ src/components/pages/cart/CartProductItem.jsx
@@ -6,25 +6,29 @@ import PropTypes from 'prop-types'
 import ButtonIcon from '../../reusables/buttons/ButtonIcon'
 import trashIcon from '../../../assets/icons/trash-2.svg'
 
-const CartProductItem = ({
-    product,
-    handleDeleteFromCart,
-}) => {
-
+const CartProductItem = ({ product, handleDeleteFromCart }) => {
     return (
         <StyledCartProductItem data-testid='cart-product-item'>
-            <Link to={`/products/${product.id}`}>
+            <Link
+                to={`/products/${product.id}`}
+                data-testid='cart-product-item__image-link'
+            >
                 <img src={product.image} alt={product.title} />
             </Link>
             <ProductItemInfo>
                 <div>
-                    <Link to={`/products/${product.id}`}>
+                    <Link
+                        to={`/products/${product.id}`}
+                        data-testid='cart-product-item__title-link'
+                    >
                         <h4>{product.title}</h4>
                     </Link>
                     <p>{formatToUSD(product.price)}</p>
                 </div>
                 <ProductItemButtonsContainer>
-                    <p>Qty: {product.quantity}</p>
+                    <p data-testid='cart-product-item__quantity'>
+                        Qty: {product.quantity}
+                    </p>
                     <ButtonIcon>
                         <img
                             src={trashIcon}
@@ -33,6 +37,7 @@ const CartProductItem = ({
                                 handleDeleteFromCart(product)
                             }}
                             title='Delete all of this item'
+                            data-testid='cart-product-item__trash-btn'
                         />
                     </ButtonIcon>
                 </ProductItemButtonsContainer>
--- src/tests/CartProductItem.test.jsx
+++ src/tests/CartProductItem.test.jsx
@@ -1,7 +1,8 @@
 import { describe, it, expect, vi } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
 import CartProductItem from '../components/pages/cart/CartProductItem'
+import userEvent from '@testing-library/user-event'
 
 const mockProduct = {
     id: 1,
@@ -15,6 +16,7 @@ const mockProduct = {
         rate: 3.9,
         count: 120,
     },
+    quantity: 1,
 }
 
 describe('Cart Product Item test', () => {
@@ -28,5 +30,70 @@ describe('Cart Product Item test', () => {
         )
         expect(screen.getByTestId('cart-product-item')).toBeInTheDocument()
         expect(screen.getByTestId('cart-product-item')).toMatchSnapshot()
+    })
+
+    it('image link click takes user to product page', async () => {
+        const user = userEvent.setup()
+        render(
+            <CartProductItem
+                product={mockProduct}
+                handleDeleteFromCart={() => {}}
+            />,
+            { wrapper: BrowserRouter },
+        )
+        const imageLink = screen.getByTestId('cart-product-item__image-link')
+
+        await user.click(imageLink)
+        await waitFor(() => {
+            expect(window.location.pathname).toBe(`/products/${mockProduct.id}`)
+        })
+    })
+
+    it('title link click takes user to product page', async () => {
+        const user = userEvent.setup()
+        render(
+            <CartProductItem
+                product={mockProduct}
+                handleDeleteFromCart={() => {}}
+            />,
+            { wrapper: BrowserRouter },
+        )
+        const titleLink = screen.getByTestId('cart-product-item__title-link')
+
+        await user.click(titleLink)
+        await waitFor(() => {
+            expect(window.location.pathname).toBe(`/products/${mockProduct.id}`)
+        })
+    })
+
+    it('title link click takes user to product page', () => {
+        render(
+            <CartProductItem
+                product={mockProduct}
+                handleDeleteFromCart={() => {}}
+            />,
+            { wrapper: BrowserRouter },
+        )
+
+        expect(
+            screen.getByTestId('cart-product-item__quantity').textContent,
+        ).toBe(`Qty: ${mockProduct.quantity}`)
+    })
+
+    it('trash button click calls handleDeleteFromCart', async () => {
+        const user = userEvent.setup()
+        const handleDeleteFromCartMock = vi.fn()
+        render(
+            <CartProductItem
+                product={mockProduct}
+                handleDeleteFromCart={handleDeleteFromCartMock}
+            />,
+            { wrapper: BrowserRouter },
+        )
+        const trashBtn = screen.getByTestId('cart-product-item__trash-btn')
+
+        await user.click(trashBtn)
+
+        expect(handleDeleteFromCartMock).toHaveBeenCalled()
     })
 })
