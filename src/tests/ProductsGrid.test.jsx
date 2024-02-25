6fe750ea6b1b1e8881f9888083fb0a891879e094
Grant Collins <grantcollins03@gmail.com>
2024-02-25 06:44:56 -0500

    added test for ProductsGrid; renders correctly

---
+++ src/tests/ProductsGrid.test.jsx
@@ -0,0 +1,40 @@
+import { describe, it, expect, vi } from 'vitest'
+import { act } from 'react-dom/test-utils'
+import { render, screen } from '@testing-library/react'
+import useData from '../hooks/useData'
+import ProductsFilterSection from '../components/pages/products/ProductsFilterSection'
+import userEvent from '@testing-library/user-event'
+import ProductsGrid from '../components/pages/products/ProductsGrid'
+import { BrowserRouter } from 'react-router-dom'
+
+// vi.mock('../hooks/useData', () => ({
+//     __esModule: true,
+//     default: vi.fn(),
+// }))
+
+const mockProducts = [
+    {
+        id: 1,
+        title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+        price: 109.95,
+        description:
+            'Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday',
+        category: "men's clothing",
+        image: 'https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg',
+        rating: {
+            rate: 3.9,
+            count: 120,
+        },
+    },
+]
+
+describe('Products Grid test', () => {
+    it('renders correctly', () => {
+        render(
+            <ProductsGrid products={mockProducts} handleAddToCart={() => {}} />,
+            { wrapper: BrowserRouter },
+        )
+        expect(screen.getByTestId('products-grid')).toBeInTheDocument()
+        expect(screen.getByTestId('products-grid')).toMatchSnapshot()
+    })
+})
