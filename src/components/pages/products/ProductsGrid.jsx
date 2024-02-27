2791e10696f71e7b22e52cb89761c84c790b1fe7
Grant Collins <grantcollins03@gmail.com>
2024-02-27 07:54:34 -0500

    moved styles below component

--- src/components/pages/products/ProductsGrid.jsx
+++ src/components/pages/products/ProductsGrid.jsx
@@ -2,19 +2,6 @@ import styled from 'styled-components'
 import ProductCard from './ProductCard'
 import PropTypes from 'prop-types'
 
-const StyledProductsGrid = styled.section`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(248px, 1fr));
-    gap: 24px;
-    /* max-width: 1400px; */
-    padding: 48px 96px;
-
-    @media (max-width: 740px) {
-        padding: 12px 16px;
-        justify-items: center;
-    }
-`
-
 const ProductsGrid = ({ products, handleAddToCart }) => {
     return (
         <StyledProductsGrid data-testid='products-grid'>
@@ -30,6 +17,19 @@ const ProductsGrid = ({ products, handleAddToCart }) => {
         </StyledProductsGrid>
     )
 }
+
+const StyledProductsGrid = styled.section`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(248px, 1fr));
+    gap: 24px;
+    /* max-width: 1400px; */
+    padding: 48px 96px;
+
+    @media (max-width: 740px) {
+        padding: 12px 16px;
+        justify-items: center;
+    }
+`
 
 ProductsGrid.propTypes = {
     products: PropTypes.array.isRequired,
